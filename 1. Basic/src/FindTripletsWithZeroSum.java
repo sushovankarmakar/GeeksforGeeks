4f40a31374c626c1e8c7938b3e8f199add5e8489
Sushovan <skarmakar@internal.tavisca.com>
2020-03-15 06:08:41 +0530

    Added the TripletSumInArray and TripletSumWithZeroSum

---
+++ 1. Basic/src/FindTripletsWithZeroSum.java
@@ -0,0 +1,74 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// https://www.geeksforgeeks.org/find-a-triplet-that-sum-to-a-given-value/
+// https://practice.geeksforgeeks.org/problems/find-triplets-with-zero-sum/1/
+
+public class FindTripletsWithZeroSum {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0) {
+			int n = Integer.parseInt(br.readLine().trim());
+			int ip[] = new int[n];
+			String s[] = br.readLine().trim().split("\\s+");
+			
+			for (int i = 0; i < n; i++) {
+				ip[i] = Integer.parseInt(s[i]);
+			}
+			System.out.println(getTripletWithZeroSum(ip, n));
+		}
+	}
+	
+	private static boolean getTripletWithZeroSum(int ip[], int n) {
+		
+		quickSort(ip, 0, n-1);
+		
+		for(int i=0; i < n -2; i++) {
+			int l = i+1;
+			int r = n-1;
+			int tripletSum = 0;
+			while(l<r) {
+				tripletSum = ip[i] + ip[l] + ip[r];
+				
+				if(tripletSum == 0) return true; 
+				if(tripletSum < 0) l++;
+				if(tripletSum > 0) r--;
+			}
+		}
+		return false;
+	}
+	
+	
+	private static void quickSort(int arr[], int low, int high) {
+		if(low < high) {
+			int pos = partition(arr, low, high);
+			quickSort(arr, low, pos-1);
+			quickSort(arr, pos+1, high);
+		}
+	}
+	
+	private static int partition(int arr[], int low, int high) {
+		int pivot = arr[high];
+		int i = low -1;
+		
+		for(int j= low; j < high; j++) {
+			if(arr[j] <= pivot) {
+				i++;
+				swap(arr, i, j);
+			}
+		}
+		swap(arr, i+1, high);
+		return i+1;
+	}
+	
+	private static void swap(int arr[], int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+}
---
+++ 2. Easy/src/TripletSumInArray.java
@@ -0,0 +1,51 @@
+package easy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// https://www.geeksforgeeks.org/find-a-triplet-that-sum-to-a-given-value/
+// https://practice.geeksforgeeks.org/problems/find-triplets-with-zero-sum/1/
+
+public class TripletSumInArray {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-- > 0) {
+			String NX[] = br.readLine().trim().split("\\s+");
+			int n = Integer.parseInt(NX[0]);
+			int x = Integer.parseInt(NX[1]);
+			int ip[] = new int[n];
+			String s[] = br.readLine().trim().split("\\s+");
+			for (int i = 0; i < s.length; i++) {
+				ip[i] = Integer.parseInt(s[i]);
+			}
+			
+			Arrays.sort(ip);
+			System.out.println(getTripletSumInArray(ip, n, x) ? "1" : "0" );
+		}
+	}
+	
+	private static boolean getTripletSumInArray(int ip[], int n, int expectedSum) {
+		
+		for (int i = 0; i < n-2; i++) {
+			int l = i+1;
+			int r = n-1;
+			
+			while(l < r) {
+				
+				int tripletSum = ip[i] + ip[l] + ip[r];
+				 
+				if( tripletSum == expectedSum ) { return true; }
+				 
+				if( tripletSum > expectedSum ) { r--; }
+				 
+				if( tripletSum < expectedSum ) { l++; } 
+			}
+		}
+		
+		return false;
+	}
+}

d5ddcd39a65f5c0af3467a4070ce73e0178e3bff
tavisca-skarmakar <51905183+tavisca-skarmakar@users.noreply.github.com>
2020-04-12 19:58:18 +0530

    Subsets.java is added

--- 2. Easy/src/PowerSet.java
+++ 2. Easy/src/PowerSet.java
@@ -3,11 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 
 // https://ide.geeksforgeeks.org/i8TJRLyL8H
 // https://practice.geeksforgeeks.org/problems/power-set/0
+// https://www.geeksforgeeks.org/finding-all-subsets-of-a-given-set-in-java/
+// https://www.geeksforgeeks.org/recursive-program-to-generate-power-set/
 
 public class PowerSet {
 
@@ -18,22 +23,25 @@ public class PowerSet {
 			int n = Integer.parseInt(br.readLine().trim());
 			String ip = br.readLine().trim();
 			printPowerSet(ip,n);
-			/*
-			 * int index = -1; String curr = ""; printPowerSet(ip, index, curr);
-			 */
+			
+			//int index = -1; String curr = ""; printPowerSet(ip, index, curr);
+			 
 		}
 	}
 
-	/*
-	 * private static void printPowerSet(String inputString, int index, String
-	 * currentString) { int n = inputString.length();
-	 * 
-	 * if (currentString != "") System.out.println(currentString);
-	 * 
-	 * for (int i = index + 1; i < n; i++) { currentString += inputString.charAt(i);
-	 * printPowerSet(inputString, i, currentString); currentString =
-	 * currentString.substring(0, currentString.length() - 1); } }
-	 */
+	
+//	private static void printPowerSet(String inputString, int index, String currentString) { 
+//		int n = inputString.length();
+//
+//		if (currentString != "") System.out.println(currentString);
+//
+//		for (int i = index + 1; i < n; i++) { 
+//			currentString += inputString.charAt(i);
+//			printPowerSet(inputString, i, currentString); 
+//			currentString = currentString.substring(0, currentString.length() - 1); 
+//		} 
+//	}
+	 
 
 	private static void printPowerSet(String ip, int n) {
 		List<String> listOfSubsets = new ArrayList<String>();
@@ -47,7 +55,7 @@ public class PowerSet {
 			if (set.length() > 0) listOfSubsets.add(set.toString());
 			set.setLength(0);
 		}
-		listOfSubsets.sort(Comparator.naturalOrder());
+		listOfSubsets.sort(Comparator.naturalOrder()); 
 		listOfSubsets.forEach(e -> System.out.print(e + " "));
 		System.out.println();
 	}

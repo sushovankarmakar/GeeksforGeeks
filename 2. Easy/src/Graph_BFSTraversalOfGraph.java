bbbc9a361f3a27f9f4dd0d7f3ad58e5e690ccead
tavisca-skarmakar <51905183+tavisca-skarmakar@users.noreply.github.com>
2020-07-09 13:33:56 +0530

    BFS Traversal Of Graph

---
+++ 2. Easy/src/Graph_BFSTraversalOfGraph.java
@@ -0,0 +1,69 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+class Graph_BFSTraversalOfGraph {
+
+	private static List<Integer> bfs(List<List<Integer>> adjList, int noOfVertexes) {
+		boolean[] isVisited = new boolean[noOfVertexes];
+		LinkedList<Integer> queue = new LinkedList<>();
+		List<Integer> listOfNodes = new ArrayList<>();
+
+		int startingNode = 0;
+		isVisited[startingNode] = true;
+		queue.add(startingNode);
+
+		while(!queue.isEmpty()) {
+			startingNode = queue.poll();
+			listOfNodes.add(startingNode);
+
+			for (int node : adjList.get(startingNode)) {
+				if (!isVisited[node]) {
+					isVisited[node] = true;
+					queue.add(node);
+				}
+			}
+		}
+		return listOfNodes;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0) {
+			String[] nm = br.readLine().trim().split("\\s+");
+			int noOfVertexes = Integer.parseInt(nm[0]);
+			int noOfEdges = Integer.parseInt(nm[1]);
+
+			String[] edges = br.readLine().trim().split("\\s+");
+			List<List<Integer>> adjList = getAdjacencyList(noOfVertexes, edges);
+
+			List<Integer> result = bfs(adjList, noOfVertexes);
+			print(result);
+		}
+	}
+
+	private static List<List<Integer>> getAdjacencyList(int noOfVertexes, String[] edges) {
+		List<List<Integer>> adjList = new ArrayList<>();
+		for (int i = 0; i < noOfVertexes; i++) {
+			adjList.add(i, new ArrayList<>());
+		}
+		for (int i = 0; i < edges.length; i+=2) {
+			int sourceNode = Integer.parseInt(edges[i]);
+			int destinationNode = Integer.parseInt(edges[i+1]);
+			adjList.get(sourceNode).add(destinationNode);	// it is a directed graph
+		}
+		return adjList;
+	}
+
+	private static void print(List<Integer> result) {
+		StringBuilder sb = new StringBuilder();
+		for (int node : result) {
+			sb.append(node + " ");
+		}
+		System.out.println();
+	}
+}

98c6d4130a5db80005393ab4dab3df60a834d17c
tavisca-skarmakar <51905183+tavisca-skarmakar@users.noreply.github.com>
2020-09-08 18:57:07 +0530

    Vertical, Top, Bottom, Level view

--- 2. Easy/src/Tree_LevelOrderTraversal.java
+++ 2. Easy/src/Tree_LevelOrderTraversal.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 // https://www.geeksforgeeks.org/level-order-tree-traversal/
@@ -9,61 +11,70 @@ import java.util.Queue;
 
 public class Tree_LevelOrderTraversal {
 	
-	static class Tree {
-		void levelOrderTraversal(Node root) {
-			if(root == null) return;
-			
-			Queue<Node> queue = new LinkedList<>();
-			queue.add(root);
-			
-			while(queue.size() > 0) {
-				Node currNode = queue.poll();
-				System.out.print(currNode.data + " ");
-				if(currNode.left != null) queue.add(currNode.left);
-				if(currNode.right != null) queue.add(currNode.right);
-			}
+	private static List<Integer> levelOrderTraversal(Node root) {
+		List<Integer> output = new ArrayList<>();
+		if (root == null) return output;
+
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(root);
+
+		while(!queue.isEmpty()) {
+			Node currNode = queue.poll();
+			output.add(currNode.data);
+
+			if(currNode.left  != null) queue.add(currNode.left);
+			if(currNode.right != null) queue.add(currNode.right);
 		}
+		return output;
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine().trim());
-		while(t-->0) {
-			String s = br.readLine().trim();
-			Node root = buildTree(s);
-			Tree tree = new Tree();
-			tree.levelOrderTraversal(root);
-			System.out.println("\n");
+		while (t-- > 0) {
+			String input = br.readLine().trim();
+			Node root = buildTree(input);
+			List<Integer> output = levelOrderTraversal(root);
+			printTree(output);
 		}
+	}
+
+	private static void printTree(List<Integer> output) {
+		StringBuilder sb = new StringBuilder();
+		for (int value : output) {
+			sb.append(value).append(" ");
+		}
+		System.out.println(sb);
+		sb.setLength(0);
 	}
 	
 	static Node buildTree(String str) {
-		if(str.length() == 0 || str.charAt(0)=='N') {
+		if (str.length() == 0 || str.charAt(0) == 'N') {
 			return null;
 		}
-		String ip[] = str.split("\\s+");
+		String[] ip = str.split("\\s+");
 		Node root = new Node(Integer.parseInt(ip[0]));
 		Queue<Node> queue = new LinkedList<>();
 		queue.add(root);
 		
 		int i = 1;
-		while(queue.size() > 0 && i < ip.length) {
+		while (!queue.isEmpty() && i < ip.length) {
 			Node currNode = queue.peek();
 			queue.remove();
 			
 			String currVal = ip[i];
 			
-			if(!currVal.equals("N")) {
+			if (!currVal.equals("N")) {
 				currNode.left = new Node(Integer.parseInt(currVal));
 				queue.add(currNode.left);
 			}
 			i++;
 			
-			if(i >= ip.length) break;
+			if (i >= ip.length) break;
 			
 			currVal = ip[i];
 			
-			if(!currVal.equals("N")) {
+			if (!currVal.equals("N")) {
 				currNode.right = new Node(Integer.parseInt(currVal));
 				queue.add(currNode.right);
 			}
--- 3. Medium/src/Tree_VerticalTraversalOfBinaryTree.java
+++ 3. Medium/src/Tree_VerticalTraversalOfBinaryTree.java
@@ -11,134 +11,135 @@ import java.util.TreeMap;
 // https://practice.geeksforgeeks.org/problems/print-a-binary-tree-in-vertical-order/1
 // https://www.geeksforgeeks.org/print-binary-tree-vertical-order-set-2/
 // https://ide.geeksforgeeks.org/sfUOjxyEfv
-
+// https://www.interviewbit.com/problems/vertical-order-traversal-of-binary-tree/
+// https://www.youtube.com/watch?v=PQKkr036wRc
+// https://www.youtube.com/watch?v=SineNp8Kjwo
+// https://practice.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1/ (Similar question)
+// https://practice.geeksforgeeks.org/problems/top-view-of-binary-tree/1/ (Similar question)
 
 public class Tree_VerticalTraversalOfBinaryTree {
 	
-	// pre order traversal didn't give right output in GCG, so I went for level order traversal
+	/* pre order traversal didn't give right output in GCG, so I went for level order traversal */
+
+	/* recursive function won't work cause there is a condition in the question which says
+	* "If there are multiple nodes passing through a vertical line,
+	* then they should be printed as they appear in level order traversal of the tree." */
 	
-	/*void verticalTraversal(Node root, int horizontalDistance, TreeMap<Integer, List<Integer>> treeMap) {
-		if(root == null) return;
+	/*
+	void verticalTraversal(Node root, int horizontalDistance, TreeMap<Integer, List<Integer>> treeMap) {
+		if (root == null) return;
 		
 		List<Integer> list = treeMap.get(horizontalDistance); 
-		if(list == null) {
+		if (list == null) {
 			list = new ArrayList<Integer>();	
 		}
 		list.add(root.data);
 		treeMap.put(horizontalDistance, list);
 		
 		
-		verticalTraversal(root.left, horizontalDistance-1, treeMap);
-		verticalTraversal(root.right, horizontalDistance+1, treeMap);
-	}*/
+		verticalTraversal(root.left, horizontalDistance - 1, treeMap);
+		verticalTraversal(root.right, horizontalDistance + 1, treeMap);
+	}
+	*/
 	
 	static class Pair {
-		Node node;
-		int horizontalDistance;
+		private TreeNode node;
+		private int horizontalDistance;
 		
-		Pair(Node node, int horizontalDistance) {
+		Pair(TreeNode node, int horizontalDistance) {
 			this.node = node;
 			this.horizontalDistance = horizontalDistance;
 		}
-		
-		Node getNode() {
-			return this.node;
-		}
-		
-		int getHorizontalDistance() {
-			return this.horizontalDistance;
-		}
 	}
-	
-	static class Tree {
-		
-		// level order traversal is applied here.
-		
-		void verticalTraversal(Node root, int horizontalDistance, TreeMap<Integer, List<Integer>> treeMap) {
-			if(root == null) return;
-			
-			Queue<Pair> queue = new LinkedList<Pair>();
-			queue.add(new Pair(root, horizontalDistance));
-			
-			while(!queue.isEmpty()) {
-				
-				int countNode = queue.size();
-				
-				while(countNode > 0) {
-					Pair pair = queue.poll();
-					
-					root = pair.getNode();
-					horizontalDistance = pair.getHorizontalDistance();
-					
-					List<Integer> list = treeMap.get(horizontalDistance); 
-					if(list == null) {
-						list = new ArrayList<Integer>();	
-					}
-					list.add(root.data);
-					treeMap.put(horizontalDistance, list);
-					
-					
-					if(root.left != null) queue.add(new Pair(root.left, horizontalDistance-1));
-					if(root.right != null) queue.add(new Pair(root.right, horizontalDistance+1));
-					
-					countNode--;
+
+	/* level order traversal is applied here */
+	/* this vertical order traversal is similar to top view and bottom view of a tree */
+
+	private static List<Integer> verticalTraversal(TreeNode root) {
+		List<Integer> output = new ArrayList<>();
+		if (root == null) return output;
+
+		Map<Integer, List<Integer>> treeMap = new TreeMap<>();	/* TreeMap is sorted according to the natural ordering of its keys */
+
+		Queue<Pair> queue = new LinkedList<>();
+		queue.add(new Pair(root, 0));
+
+		while (!queue.isEmpty()) {
+			int queueSize = queue.size();
+
+			while (queueSize-- > 0) {
+
+				Pair pair = queue.poll();
+				TreeNode currNode = pair.node;
+				int horizontalDistance = pair.horizontalDistance;
+
+				List<Integer> list = treeMap.get(horizontalDistance);
+
+				if (list == null) {
+					list = new ArrayList<>();
 				}
-			}	
+
+				list.add(currNode.data);
+				treeMap.put(horizontalDistance, list);
+
+				if (currNode.left  != null) queue.add(new Pair(currNode.left,  horizontalDistance - 1));
+				if (currNode.right != null) queue.add(new Pair(currNode.right, horizontalDistance + 1));
+			}
 		}
-		
-		void printTree(TreeMap<Integer, List<Integer>> treeMap) {
-			for (Map.Entry<Integer, List<Integer>> entry : treeMap.entrySet()) { 
-	            List<Integer> list  = (List<Integer>) entry.getValue();
-	            for (Integer data : list) {
-					System.out.print(data + " ");
-				} 
-	        }
-			System.out.println("\n");
+
+		for (List<Integer> list : treeMap.values()) {
+			output.addAll(list);
 		}
+		return output;
 	}
-	
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine().trim());
-		while(t-->0) {
+		while (t-- > 0) {
 			String s = br.readLine().trim();
-			Node root = buildTree(s);
-			Tree tree = new Tree();
-			int horizontalDistance = 0; 
-			TreeMap<Integer, List<Integer>> treeMap = new TreeMap<>();
-			tree.verticalTraversal(root, horizontalDistance, treeMap);
-			tree.printTree(treeMap);
+			TreeNode root = buildTree(s);
+			List<Integer> output = verticalTraversal(root);
+			printTree(output);
 		}
 	}
+
+	private static void printTree(List<Integer> output) {
+		StringBuilder sb = new StringBuilder();
+		for (int value : output) {
+			sb.append(value).append(" ");
+		}
+		System.out.println(sb);
+		sb.setLength(0);
+	}
 	
-	static Node buildTree(String str) {
-		if(str.length() == 0 || str.charAt(0)=='N') return null;
+	private static TreeNode buildTree(String str) {
+		if (str.length() == 0 || str.charAt(0) == 'N') return null;
 		
-		String ip[] = str.split("\\s+");
-		Node root = new Node(Integer.parseInt(ip[0]));
-		Queue<Node> queue = new LinkedList<>();
+		String[] ip = str.split("\\s+");
+		TreeNode root = new TreeNode(Integer.parseInt(ip[0]));
+		Queue<TreeNode> queue = new LinkedList<>();
 		queue.add(root);
 		
 		int i = 1;
-		while (queue.size() > 0 && i < ip.length) {
-			
-			Node currNode = queue.poll();
+		while (!queue.isEmpty() && i < ip.length) {
+
+			TreeNode currNode = queue.poll();
 			
 			String currVal = ip[i];
 			
-			if(!currVal.equals("N")) {
-				currNode.left = new Node(Integer.parseInt(currVal));
+			if (!currVal.equals("N")) {
+				currNode.left = new TreeNode(Integer.parseInt(currVal));
 				queue.add(currNode.left);
 				
 			}
 			i++;
 			
-			if(i >= ip.length) break;
+			if (i >= ip.length) break;
 			
 			currVal = ip[i];
-			if(!currVal.equals("N")) {
-				currNode.right = new Node(Integer.parseInt(currVal));
+			if (!currVal.equals("N")) {
+				currNode.right = new TreeNode(Integer.parseInt(currVal));
 				queue.add(currNode.right);
 			}
 			i++;
@@ -146,11 +147,11 @@ public class Tree_VerticalTraversalOfBinaryTree {
 		return root;
 	}
 	
-	static class Node {
-		int data;
-		Node left;
-		Node right;
-		Node(int data) {
+	private static class TreeNode {
+		private int data;
+		private TreeNode left;
+		private TreeNode right;
+		TreeNode(int data) {
 			this.data = data;
 			left = right = null;
 		}
